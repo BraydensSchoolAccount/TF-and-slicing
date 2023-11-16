e5fa254b1ceb6d6f5e17341f1610c92a6b73d13f
brayden <bt22219@students.hcde.org>
2023-11-16 07:40:37 -0500

    first commit

---
+++ TF and slicing.py
@@ -0,0 +1,18 @@
+# True/False and slicing
+# Brayden Towner
+# 11/16/2023
+
+greeting = input("Hello! (Hint: say hello back) > ")
+# Using lower (we did learn this) to make comparisons easier
+if greeting.lower() == "hello":
+    # Just a silly, not actually making an rpg out of this
+    print("How are you? TODO: FINISH EPIC RPG")
+else:
+    print("You could at least say HELLO!")
+
+ing_check = input("Just write a word with \"ing\" at the end >  ")
+
+if ing_check[-3:].lower() == "ing":
+    print("Thank you!")
+else:
+    print(">:(")
